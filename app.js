97f34a5b0dae0645b2935c71f1f84ee10e357106
zinzoddari <zinzoddari@gmail.com>
2019-07-14 18:41:59 +0900

    Database connetion complete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//필요한 모듈을 선언함
 // express 모듈 불러온 후 express 변수에 저장
 // npm install express -save 명령어로 express 모듈을 활용하여 Node.js 서버 개발 위함
 // 설치한 express 모듈을 불러옴
@@ -16,59 +17,13 @@ var app = express();
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
-// http://localhost:9090/keyboard
-app.get('/keyboard', function(req, res) {
-    console.log("GO!");
-    //전달할 데이터
-    var data = {
-        'type' : 'buttons',
-        'buttons' : ['과일', '채소', '정보']
-    };
+//라우팅 모듈 선언
+var indexRouter = require('./routes/message');
 
-    // JSON 형식으로 응답
-    res.json(data);
- });
+//request 요청 URL과 처리 로직을 선언한 라우팅 모듈 매핑
+app.use('/', indexRouter);
 
- app.post('/message', function(req, res){
-     //유저가 대화방에 입력한 데이터
-
-     //req : 요청 객체
-     //body : POST 데이터는 body에 포함되어 전달
-     //content : API문서에는 content는 사용자가 입력한 데이터라고 명시 
-     var msg = req.body.msg; //msg를 카카오톡 API연동시 content로 수정하세요.
-     console.log('전달받은 메세지 : ' + msg);
-     
-     //사용자에게 응답할 데이터
-     var send = {};
-
-     switch(msg) {
-         case '과일' :
-            send = {
-                'message' : {
-                    'text' : '과일을 선택하였습니다.'
-                }
-            }
-            break;
-
-        default :
-            send = {
-
-            }
-            break;
-     }
-
-     //send 변수에 저장된 데이터 전달
-     res.json(send);
- });
-
- // 9090포트로 서버 실행
+//9090포트로 서버 생성 및 실행
  http.createServer(app).listen(9090, function(){
-     console.log("STARTING SERVER...");
- });
-
- //app.get() 라우팅 처리란?
- //https://www.naver.com/login
- //https://www.naver.com/search 등의 url 접속시 어떻게 응답을 할지에 대한 네비게이션
- //data변수는 위 주소로 접속했을 때 제공할 데이터
-
- //post 데이터를 쉽게 읽고 사용하기 위하여 npm install body-parser --save를 이용
+    console.log("STARTING SERVER...");
+});
---
+++ routes/message.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql_dbc = require('../conf/db_con')();
+var connection = mysql_dbc.init();
+mysql_dbc.test_open(connection);
+
+// http://localhost:9090/keyboard
+router.get('/', function(req, res) {
+    console.log("!!!here");
+ });
+
+ router.post('/message', function(req, res){
+     
+    //유저가 대화방에 입력한 데이터
+
+     //req : 요청 객체
+     //body : POST 데이터는 body에 포함되어 전달
+     //content : API문서에는 content는 사용자가 입력한 데이터라고 명시 
+     var msg = req.body.msg; //msg를 카카오톡 API연동시 content로 수정하세요.
+     console.log('전달받은 메세지 : ' + msg);
+     
+     //사용자에게 응답할 데이터
+     var send = {};
+
+     switch(msg) {
+         case '과일' :
+            send = {
+                'message' : {
+                    'text' : '과일을 선택하였습니다.'
+                }
+            }
+            break;
+
+        default :
+            send = {
+
+            }
+            break;
+     }
+
+     //send 변수에 저장된 데이터 전달
+     res.json(send);
+ });
+
+ //모듈에 등록해야 app.js에서 app.use 함수를 통해서 사용 가능
+ module.exports = router;
+
+
+ //app.get() 라우팅 처리란?
+ //https://www.naver.com/login
+ //https://www.naver.com/search 등의 url 접속시 어떻게 응답을 할지에 대한 네비게이션
+ //data변수는 위 주소로 접속했을 때 제공할 데이터
+
+ //post 데이터를 쉽게 읽고 사용하기 위하여 npm install body-parser --save를 이용
